6788f0acfde22820ffbf010330d51ffcb9c688b3
wgibson002 <91209879+wgibson002@users.noreply.github.com>
2023-09-03 16:36:33 -0600

    Initial Commit

---
+++ Parallel Primes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Parallel Primes
+//
+//  Created by Will Gibson on 9/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
